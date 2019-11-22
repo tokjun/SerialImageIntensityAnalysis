c59ca354568ce0fd366ef7f9ac0fc1c71d1838ac
Junichi Tokuda <tokuda@bwh.harvard.edu>
2019-11-22 14:19:54 -0500

    Update GUI

--- IntensitySampling/IntensitySampling.py
+++ IntensitySampling/IntensitySampling.py
@@ -16,9 +16,9 @@ class IntensitySampling(ScriptedLoadableModule):
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
     self.parent.title = "IntensitySampling" # TODO make this more human readable by adding spaces
-    self.parent.categories = ["Examples"]
+    self.parent.categories = ["Sequences"]
     self.parent.dependencies = []
-    self.parent.contributors = ["John Doe (AnyWare Corp.)"] # replace with "Firstname Lastname (Organization)"
+    self.parent.contributors = ["Junichi Tokuda (BWH)"] # replace with "Firstname Lastname (Organization)"
     self.parent.helpText = """
 This is an example of scripted loadable module bundled in an extension.
 It performs a simple thresholding on the input volume and optionally captures a screenshot.
@@ -41,6 +41,26 @@ class IntensitySamplingWidget(ScriptedLoadableModuleWidget):
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
 
+    ####################
+    # For debugging
+    #
+    # Reload and Test area
+    reloadCollapsibleButton = ctk.ctkCollapsibleButton()
+    reloadCollapsibleButton.text = "Reload && Test"
+    self.layout.addWidget(reloadCollapsibleButton)
+    reloadFormLayout = qt.QFormLayout(reloadCollapsibleButton)
+    
+    # reload button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadButton = qt.QPushButton("Reload")
+    self.reloadButton.toolTip = "Reload this module."
+    self.reloadButton.name = "CurveMaker Reload"
+    reloadFormLayout.addWidget(self.reloadButton)
+    self.reloadButton.connect('clicked()', self.onReload)
+    #
+    ####################
+
     # Instantiate and connect widgets ...
 
     #
@@ -54,10 +74,10 @@ class IntensitySamplingWidget(ScriptedLoadableModuleWidget):
     parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
 
     #
-    # input volume selector
+    # Input sequence node selector
     #
     self.inputSelector = slicer.qMRMLNodeComboBox()
-    self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.inputSelector.nodeTypes = ["vktMRMLSequenceNode"]
     self.inputSelector.selectNodeUponCreation = True
     self.inputSelector.addEnabled = False
     self.inputSelector.removeEnabled = False
@@ -65,42 +85,65 @@ class IntensitySamplingWidget(ScriptedLoadableModuleWidget):
     self.inputSelector.showHidden = False
     self.inputSelector.showChildNodeTypes = False
     self.inputSelector.setMRMLScene( slicer.mrmlScene )
-    self.inputSelector.setToolTip( "Pick the input to the algorithm." )
-    parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
+    self.inputSelector.setToolTip( "Select a sequence node that contains a serial image." )
+    parametersFormLayout.addRow("Input Image Sequence: ", self.inputSelector)
 
     #
-    # output volume selector
+    # Input Label node selector
     #
-    self.outputSelector = slicer.qMRMLNodeComboBox()
-    self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
-    self.outputSelector.selectNodeUponCreation = True
-    self.outputSelector.addEnabled = True
-    self.outputSelector.removeEnabled = True
-    self.outputSelector.noneEnabled = True
-    self.outputSelector.showHidden = False
-    self.outputSelector.showChildNodeTypes = False
-    self.outputSelector.setMRMLScene( slicer.mrmlScene )
-    self.outputSelector.setToolTip( "Pick the output to the algorithm." )
-    parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
+    self.inputLabelSelector = slicer.qMRMLNodeComboBox()
+    self.inputLabelSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+    self.inputLabelSelector.selectNodeUponCreation = True
+    self.inputLabelSelector.addEnabled = False
+    self.inputLabelSelector.removeEnabled = False
+    self.inputLabelSelector.noneEnabled = False
+    self.inputLabelSelector.showHidden = False
+    self.inputLabelSelector.showChildNodeTypes = False
+    self.inputLabelSelector.setMRMLScene( slicer.mrmlScene )
+    self.inputLabelSelector.setToolTip( "Select a sequence node that contains a serial image." )
+    parametersFormLayout.addRow("Input Label: ", self.inputLabelSelector)
 
-    #
-    # threshold value
-    #
-    self.imageThresholdSliderWidget = ctk.ctkSliderWidget()
-    self.imageThresholdSliderWidget.singleStep = 0.1
-    self.imageThresholdSliderWidget.minimum = -100
-    self.imageThresholdSliderWidget.maximum = 100
-    self.imageThresholdSliderWidget.value = 0.5
-    self.imageThresholdSliderWidget.setToolTip("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero.")
-    parametersFormLayout.addRow("Image threshold", self.imageThresholdSliderWidget)
+    
+    ##
+    ## output file Selector
+    ##
+    outputFileLayout = qt.QHBoxLayout()
+    
+    self.outputFileEdit = qt.QLineEdit()
+    self.outputFileEdit.text = ''
+    self.outputFileEdit.readOnly = True
+    self.outputFileEdit.frame = True
+    self.outputFileEdit.styleSheet = "QLineEdit { background:transparent; }"
+    self.outputFileEdit.cursor = qt.QCursor(qt.Qt.IBeamCursor)
 
-    #
-    # check box to trigger taking screen shots for later use in tutorials
-    #
-    self.enableScreenshotsFlagCheckBox = qt.QCheckBox()
-    self.enableScreenshotsFlagCheckBox.checked = 0
-    self.enableScreenshotsFlagCheckBox.setToolTip("If checked, take screen shots for tutorials. Use Save Data to write them to disk.")
-    parametersFormLayout.addRow("Enable Screenshots", self.enableScreenshotsFlagCheckBox)
+    self.outputFileBrowserButton = qt.QPushButton()
+    self.outputFileBrowserButton.setText("...")
+
+    self.outputFileDialog = qt.QFileDialog()
+    
+    outputFileLayout.addWidget(self.outputFileEdit)
+    outputFileLayout.addWidget(self.outputFileBrowserButton)
+    parametersFormLayout.addRow("Output File (CSV): ", outputFileLayout)
+
+    #self.outputSelector = slicer.qMRMLNodeComboBox()
+    #self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    #self.outputSelector.selectNodeUponCreation = True
+    #self.outputSelector.addEnabled = True
+    #self.outputSelector.removeEnabled = True
+    #self.outputSelector.noneEnabled = True
+    #self.outputSelector.showHidden = False
+    #self.outputSelector.showChildNodeTypes = False
+    #self.outputSelector.setMRMLScene( slicer.mrmlScene )
+    #self.outputSelector.setToolTip( "Pick the output to the algorithm." )
+    #parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
+
+    ##
+    ## check box to trigger taking screen shots for later use in tutorials
+    ##
+    #self.enableScreenshotsFlagCheckBox = qt.QCheckBox()
+    #self.enableScreenshotsFlagCheckBox.checked = 0
+    #self.enableScreenshotsFlagCheckBox.setToolTip("If checked, take screen shots for tutorials. Use Save Data to write them to disk.")
+    #parametersFormLayout.addRow("Enable Screenshots", self.enableScreenshotsFlagCheckBox)
 
     #
     # Apply Button
@@ -113,7 +156,8 @@ class IntensitySamplingWidget(ScriptedLoadableModuleWidget):
     # connections
     self.applyButton.connect('clicked(bool)', self.onApplyButton)
     self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
-    self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    #self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.outputFileBrowserButton.connect('clicked(bool)', self.onInputFileBrowser)
 
     # Add vertical spacer
     self.layout.addStretch(1)
@@ -125,27 +169,33 @@ class IntensitySamplingWidget(ScriptedLoadableModuleWidget):
     pass
 
   def onSelect(self):
-    self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
+    #self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
+    pass
 
   def onApplyButton(self):
     logic = IntensitySamplingLogic()
-    enableScreenshotsFlag = self.enableScreenshotsFlagCheckBox.checked
-    imageThreshold = self.imageThresholdSliderWidget.value
-    logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode(), imageThreshold, enableScreenshotsFlag)
+    #enableScreenshotsFlag = self.enableScreenshotsFlagCheckBox.checked
+    #logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode(), imageThreshold, enableScreenshotsFlag)
 
+
+  def onInputFileSelector(self):
+    outputFilePath = self.outputFileDialog.getExistingDirectory()
+
+    if self.savingSeperateChannelCheckBox.checked == True:
+      self.importDirString = self.fileDialog.getExistingDirectory()
+      if len(self.importDirString) > self.maximumFileNameLen:
+        self.inputFileBrowserButton.setText(".." + self.importDirString[-self.maximumFileNameLen:])
+    else:
+      self.fileString = self.fileDialog.getOpenFileName()
+      if len(self.fileString)>self.maximumFileNameLen:
+        self.inputFileBrowserButton.setText(".." + self.fileString[-self.maximumFileNameLen:])
+    
+    
 #
 # IntensitySamplingLogic
 #
 
 class IntensitySamplingLogic(ScriptedLoadableModuleLogic):
-  """This class should implement all the actual
-  computation done by your module.  The interface
-  should be such that other python code can import
-  this class and make use of the functionality without
-  requiring an instance of the Widget.
-  Uses ScriptedLoadableModuleLogic base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
 
   def hasImageData(self,volumeNode):
     """This is an example logic method that
@@ -189,10 +239,6 @@ class IntensitySamplingLogic(ScriptedLoadableModuleLogic):
     cliParams = {'InputVolume': inputVolume.GetID(), 'OutputVolume': outputVolume.GetID(), 'ThresholdValue' : imageThreshold, 'ThresholdType' : 'Above'}
     cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True)
 
-    # Capture screenshot
-    if enableScreenshots:
-      self.takeScreenshot('IntensitySamplingTest-Start','MyScreenshot',-1)
-
     logging.info('Processing completed')
 
     return True
@@ -214,33 +260,33 @@ class IntensitySamplingTest(ScriptedLoadableModuleTest):
     """Run as few or as many tests as needed here.
     """
     self.setUp()
-    self.test_IntensitySampling1()
-
-  def test_IntensitySampling1(self):
-    """ Ideally you should have several levels of tests.  At the lowest level
-    tests should exercise the functionality of the logic with different inputs
-    (both valid and invalid).  At higher levels your tests should emulate the
-    way the user would interact with your code and confirm that it still works
-    the way you intended.
-    One of the most important features of the tests is that it should alert other
-    developers when their changes will have an impact on the behavior of your
-    module.  For example, if a developer removes a feature that you depend on,
-    your test should break so they know that the feature is needed.
-    """
-
-    self.delayDisplay("Starting the test")
-    #
-    # first, get some data
-    #
-    import SampleData
-    SampleData.downloadFromURL(
-      nodeNames='FA',
-      fileNames='FA.nrrd',
-      uris='http://slicer.kitware.com/midas3/download?items=5767',
-      checksums='SHA256:12d17fba4f2e1f1a843f0757366f28c3f3e1a8bb38836f0de2a32bb1cd476560')
-    self.delayDisplay('Finished with download and loading')
-
-    volumeNode = slicer.util.getNode(pattern="FA")
-    logic = IntensitySamplingLogic()
-    self.assertIsNotNone( logic.hasImageData(volumeNode) )
-    self.delayDisplay('Test passed!')
+    #self.test_IntensitySampling1()
+    
+  #def test_IntensitySampling1(self):
+  #  """ Ideally you should have several levels of tests.  At the lowest level
+  #  tests should exercise the functionality of the logic with different inputs
+  #  (both valid and invalid).  At higher levels your tests should emulate the
+  #  way the user would interact with your code and confirm that it still works
+  #  the way you intended.
+  #  One of the most important features of the tests is that it should alert other
+  #  developers when their changes will have an impact on the behavior of your
+  #  module.  For example, if a developer removes a feature that you depend on,
+  #  your test should break so they know that the feature is needed.
+  #  """
+  #
+  #  self.delayDisplay("Starting the test")
+  #  #
+  #  # first, get some data
+  #  #
+  #  import SampleData
+  #  SampleData.downloadFromURL(
+  #    nodeNames='FA',
+  #    fileNames='FA.nrrd',
+  #    uris='http://slicer.kitware.com/midas3/download?items=5767',
+  #    checksums='SHA256:12d17fba4f2e1f1a843f0757366f28c3f3e1a8bb38836f0de2a32bb1cd476560')
+  #  self.delayDisplay('Finished with download and loading')
+  #
+  #  volumeNode = slicer.util.getNode(pattern="FA")
+  #  logic = IntensitySamplingLogic()
+  #  self.assertIsNotNone( logic.hasImageData(volumeNode) )
+  #  self.delayDisplay('Test passed!')
